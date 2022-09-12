cfff7837c143fb6b5e98ec71252ec06942a1ea2c
hrkmtsmt <hrkmtsmt@icloud.com>
2022-09-13 00:24:50 +0900

    :+1: [FEATURE]: Create Todo entity

--- api/src/todo/entities/todo.entity.ts
+++ api/src/todo/entities/todo.entity.ts
@@ -1,1 +1,24 @@
-export class Todo {}
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Todo {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  body: string;
+
+  @Column()
+  isCompleted: boolean;
+
+  @CreateDateColumn()
+  createdAt: string;
+}
