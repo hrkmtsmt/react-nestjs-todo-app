e9c259ebe2c1efc489ce9253fc4256ff0d327098
hrkmtsmt <hrkmtsmt@icloud.com>
2022-09-13 00:12:36 +0900

    :+1: [FEATURE]: Create Todo resource

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
